d8a28343127ef95077cf4262172a01f6e4af7d21
nageshwaran08 <60385420+nageshwaran08@users.noreply.github.com>
2020-04-30 10:59:45 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,14 +5,11 @@ echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 echo 1 > /proc/sys/net/ipv6/conf/default/disable_ipv6
 yum -y update
-#yum -y install httpd mysql mysql-server mysql-devel php php-mysql php-fpm;
 yum -y install httpd mariadb mariadb-server php php-mysql php-fpm
 yum -y install epel-release
-#yum -y install php-mbstring php-gd httpd-devel apxs php-mcrypt php-mysql pdo-mysql php-sqlite php-mcrypt php-soap
 yum -y install php-mbstring php-gd httpd-devel php-mcrypt php-mysql php-ZendFramework-Db-Adapter-Pdo-Mysql sqlite php-mcrypt php-soap
 yum -y install rpm-build redhat-rpm-config
 yum -y install telnet-server telnet nmap
-#yum -y install mysql-community-release-el7-5.noarch.rpm proj
 yum -y install proj
 yum -y install tinyxml 
 yum -y install mod_perl perl-Archive-Zip perl-CPAN perl-YAML
